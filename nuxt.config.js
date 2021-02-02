14516a276713d9f1c4f159d1cde6774aaafedf75
astadler <adrien.stadler@gmail.com>
2021-02-02 14:42:27 +0100

    :sparkles: add meta tags for images

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,13 +20,15 @@ export default {
       { hid: 'author', name: 'author', content: 'Waspische' },
       { hid: 'description', name: 'copyright', content: 'My own copyright' },
       { hid: 'og:type', property: 'og:type', content: 'wbesite' },
-      { hid: 'og:type', property: 'og:url', content: 'https://waspische.github.io/Earth2Biomes/' },
-      { hid: 'og:type', property: 'og:title', content: 'Earth2Biomes' },
-      { hid: 'og:type', property: 'og:description', content: 'Discover the differents resources available on Earth1 to buy on Earth2.' },
-      { hid: 'og:type', property: 'twitter:card', content: 'summary_large_image' },
-      { hid: 'og:type', property: 'twitter:url', content: 'https://waspische.github.io/Earth2Biomes/' },
-      { hid: 'og:type', property: 'twitter:title', content: 'Earth2Biomes' },
-      { hid: 'og:type', property: 'twitter:description', content: 'Discover the differents resources available on Earth1 to buy on Earth2.' }
+      { hid: 'og:url', property: 'og:url', content: 'https://waspische.github.io/Earth2Biomes/' },
+      { hid: 'og:title', property: 'og:title', content: 'Earth2Biomes' },
+      { hid: 'og:description', property: 'og:description', content: 'Discover the differents resources available on Earth1 to buy on Earth2.' },
+      { hid: 'og:image', property: 'og:image', content: 'https://waspische.github.io/Earth2Biomes/meta_image.png' },
+      { hid: 'og:card', property: 'twitter:card', content: 'summary_large_image' },
+      { hid: 'og:url', property: 'twitter:url', content: 'https://waspische.github.io/Earth2Biomes/' },
+      { hid: 'og:title', property: 'twitter:title', content: 'Earth2Biomes' },
+      { hid: 'og:description', property: 'twitter:description', content: 'Discover the differents resources available on Earth1 to buy on Earth2.' },
+      { hid: 'og:image', property: 'twitter:image', content: 'https://waspische.github.io/Earth2Biomes/meta_image.png' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico?v=2' },
