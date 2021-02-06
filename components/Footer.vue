8c3b8d0afc3034356835fa7391b53e8071317853
astadler <adrien.stadler@gmail.com>
2021-02-06 12:19:17 +0100

    :sparkles: update referal code

--- components/Footer.vue
+++ components/Footer.vue
@@ -14,7 +14,7 @@
         class="primary darken-1 py-4 text-center white--text"
         cols="12"
       >
-        The data comes from openstreetmap.org. If you like this project, support me using the code <strong>19DBDIXSCW</strong> or <a target="_blank" rel="noopener noreferrer" href="https://ko-fi.com/earth2biomes" @click="trackFooterClick('kofi')">buy me a coffee ☕️</a>
+        If you like this project, support me using the code <strong>HABN6K6K3X</strong> or <a target="_blank" rel="noopener noreferrer" href="https://ko-fi.com/earth2biomes" @click="trackFooterClick('kofi')">buy me a coffee ☕️</a>
         <br>
         To submit an idea/bug, you can
         <a href="https://forms.gle/8Ak6HTaVh5hFyf4Q9" target="_blank" rel="noopener noreferrer" @click="trackFooterClick('form')">
